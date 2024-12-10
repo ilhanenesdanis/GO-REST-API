0679fac492d0ec158be49596ddee50648e6f32a4
ilhan enes daniş <danisenes3@gmail.com>
2024-12-10 13:30:15 +0300

    get & create events

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"REST-API/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,10 +11,29 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents) //GET, POST,PUT,PATCH,DELETE
+	server.POST("/events", createEvents)
 
 	server.Run(":8080") //localhost:8080
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
+
+	events := models.GetAllEvents()
+
+	context.JSON(http.StatusOK, events)
+}
+func createEvents(context *gin.Context) {
+
+	var event models.Event
+
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Validating error"})
+		return
+	}
+	event.ID = 1
+	event.UserID = 1
+
+	context.JSON(http.StatusCreated, gin.H{"message": "event created", "event": event})
 }
